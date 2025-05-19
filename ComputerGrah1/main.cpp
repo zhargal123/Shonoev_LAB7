7ba76653713b182cec7120009eb32c537828cc85
zhargal123 <zargalsonoev449@gmail.com>
2025-05-19 12:28:11 +0800

    Добавьте файлы проекта.

---
+++ ComputerGrah1/main.cpp
@@ -0,0 +1,223 @@
+#include <glew.h>
+#include <glfw3.h>
+#include <iostream>
+#include <string>
+
+#include "Shader.h"
+#include "Model.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+const unsigned int SCR_WIDTH = 1024;
+const unsigned int SCR_HEIGHT = 768;
+
+glm::vec3 cameraPos = glm::vec3(2.0f, 1.0f, 4.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float yaw = -90.0f;
+float pitch = 0.0f;
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+glm::mat4 rotateOX1 = glm::mat4(1.0f);
+glm::mat4 rotateOX2 = glm::mat4(1.0f);
+glm::mat4 rotateOX3 = glm::mat4(1.0f);
+
+float angleOX1 = 0.0f;
+float angleOX2 = 0.0f;
+float angleOX3 = 0.0f;
+
+void processInput(GLFWwindow* window)
+{
+    float cameraSpeed = 2.5f * deltaTime;
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+    if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS) {
+        if (angleOX1 < 0.488) {
+            float NewAngle = angleOX1 + deltaTime;
+            rotateOX1 = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, NewAngle));
+            angleOX1 = NewAngle;
+        }
+    }
+    if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS) {
+        if (angleOX1 > 0.0) {
+            float NewAngle = angleOX1 - deltaTime;
+            rotateOX1 = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, NewAngle));
+            angleOX1 = NewAngle;
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) {
+        if (angleOX2 > -0.45) {
+            float NewAngle = angleOX2 - deltaTime;
+            rotateOX2 = glm::translate(glm::mat4(1.0f), glm::vec3(NewAngle, 0.0f, 0.0f));
+            angleOX2 = NewAngle;
+        }
+    }
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) {
+        if (angleOX2 < 0.0) {
+            float NewAngle = angleOX2 + deltaTime;
+            rotateOX2 = glm::translate(glm::mat4(1.0f), glm::vec3(NewAngle, 0.0f, 0.0f));
+            angleOX2 = NewAngle;
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) {
+        if (angleOX3 > -0.4) {
+            float NewAngle = angleOX3 - deltaTime;
+            rotateOX3 = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, NewAngle, 0.0f));
+            angleOX3 = NewAngle;
+        }
+    }
+    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) {
+        if (angleOX3 < 0.0) {
+            float NewAngle = angleOX3 + deltaTime;
+            rotateOX3 = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, NewAngle, 0.0f));
+            angleOX3 = NewAngle;
+        }
+    }
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "3D Model Viewer", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+    Shader ourShader("vertex_shader.glsl", "fragment_shader.glsl");
+
+    // Загрузка модели
+    Model ourModel("asset\\Model.obj");
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput(window);
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        ourShader.Use();
+
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1.0f));
+
+        glm::mat3 normalMatrix = glm::transpose(glm::inverse(glm::mat3(model)));
+        ourShader.SetUniform("normalMatrix", glm::value_ptr(normalMatrix));
+        glm::vec3 lightColor(0.8f, 0.8f, 0.8f);
+        ourShader.SetUniform("lightColor", lightColor.x, lightColor.y, lightColor.z);
+
+        // Материал
+        ourShader.SetUniform("material.ambient", 1.0f, 0.5f, 0.31f);
+        ourShader.SetUniform("material.diffuse", 1.0f, 0.5f, 0.31f);
+        ourShader.SetUniform("material.specular", 0.5f, 0.5f, 0.5f);
+        ourShader.SetUniform("material.shininess", 32.0f);
+        ourShader.SetUniform("viewPos", cameraPos.x, cameraPos.y, cameraPos.z);
+
+        // Матрицы трансформаций
+        ourShader.SetUniform("projection", glm::value_ptr(projection));
+        ourShader.SetUniform("view", glm::value_ptr(view));
+        ourShader.SetUniform("model", glm::value_ptr(model));
+
+        // Отрисовка модели
+        ourModel.Draw(ourShader, rotateOX1, rotateOX2, rotateOX3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
